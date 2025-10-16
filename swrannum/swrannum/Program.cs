3af265c8534158ea16fe537b83bf86654d733fbf
sasiski112 <mshevelovv@gmail.com>
2025-10-16 11:12:48 +0300

    yuio

--- swrannum/swrannum/Program.cs
+++ swrannum/swrannum/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            int number = 
+            Console.WriteLine("Your number is: ");
+            int number = new Random().Next(1, 6);
+            
+            switch (number)
+                {
+                case 1:
+                    Console.WriteLine(number);
+                    break;
+                case 2:
+                    Console.WriteLine(number);
+                    break;
+                case 3:
+                    Console.WriteLine(number);
+                    break;
+                case 4:
+                    Console.WriteLine(number);
+                    break;
+                case 5:
+                    Console.WriteLine(number);
+                    break;
+                case 6:
+                    Console.WriteLine(number);
+                    break;
+                   default:
+                    Console.WriteLine("Wrong one");
+                    break;
+            }
+           
         }
     }
 }
