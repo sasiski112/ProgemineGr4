72880fa3189d8229ae83ca7e62d650e289c43e38
sasiski112 <mshevelovv@gmail.com>
2025-10-16 11:44:54 +0300

    heli

---
+++ 3numb/3numb/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _3numb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+                {
+                case 1:
+                    Console.WriteLine("1");
+                    Console.Beep(567, 647);
+                    break;
+                    case 2:
+                    Console.WriteLine("2");
+                    Console.Beep(895, 121);
+                    break;
+                    case 3:
+                    Console.WriteLine("3");
+                    Console.Beep(563, 823);
+                    break;
+                    case 4:
+                    Console.WriteLine("4");
+                    Console.Beep(1000, 500);
+                    Console.Beep(1000, 500);
+                    break;  
+            
+            
+            
+            }
+        }
+    }
+}
